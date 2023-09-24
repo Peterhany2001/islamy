78721c1661c4c5148e8097e5ac8043028329e582
user <pepohany2001@gmail.com>
2023-09-24 18:31:43 +0300

    Finish design sabha

--- lib/ui/screens/home/tabs/sebha_tab/sebha_tab.dart
+++ lib/ui/screens/home/tabs/sebha_tab/sebha_tab.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:islamy/provider/setting_provider.dart';
 import 'package:islamy/ui/utils/app_assets.dart';
+import 'package:islamy/ui/utils/app_colors.dart';
+import 'package:provider/provider.dart';
 
 class SebhaTab extends StatefulWidget {
   @override
@@ -9,52 +12,59 @@ class SebhaTab extends StatefulWidget {
 class _SebhaTabState extends State<SebhaTab> {
   @override
   Widget build(BuildContext context) {
+    SettingProvider provider = Provider.of(context);
     return Center(
-      child: Padding(
-        padding: const EdgeInsets.only(top: 30),
-        child: Column(
-          children: [
-            Image.asset(AppAssets.sebhaHead),
-            Image.asset(AppAssets.sebha),
-            Padding(
-              padding: const EdgeInsets.all(30),
-              child: Text(
-                "عدد التسبيحات",
-                style: Theme.of(context).textTheme.titleLarge,
-              ),
-            ),
-            Container(
-              width: 70,
-              height: 50,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20), color: Colors.blue),
-              child: Center(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('33', style: Theme.of(context).textTheme.titleLarge),
-                  ],
+      child: Column(
+        children: [
+          Image.asset(AppAssets.sebhaHead),
+          Transform.rotate(
+            angle: 2,
+            child: Container(
+              width: 250,
+              height: 250,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(AppAssets.sebha),
                 ),
               ),
             ),
-            // comment
-            Container(
-              width: 70,
-              height: 50,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(20), color: Colors.blue),
-              child: Center(
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('33', style: Theme.of(context).textTheme.titleLarge),
-                  ],
-                ),
-              ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(30),
+            child: Text(
+              "عدد التسبيحات",
+              style: Theme.of(context).textTheme.titleLarge,
             ),
+          ),
+          Container(
+            width: 70,
+            height: 60,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),color: provider.currentTheme==ThemeMode.light
+                ? AppColors.primiary
+                : AppColors.primiaryDark),
+            child: Center(
+              child: Text('33', style: Theme.of(context).textTheme.titleLarge),
+            ),
+          ),
+          // comment
 
-          ],
-        ),
+          Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Container(
+              width: 140 ,
+              height: 60,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30), color: provider.currentTheme==ThemeMode.light
+                  ? AppColors.primiary
+                  : AppColors.accentDark),
+              child: Center(
+                child: Text('سبحان الله', style: Theme.of(context).textTheme.titleLarge),
+              ),
+            ),
+          ),
+
+        ],
       ),
     );
   }
